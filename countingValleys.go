39cfbacf7a154d2acfc6b4be8411b441633f540f
Aditya <rarifky.aditya@gmail.com>
2020-08-12 01:45:20 +0700

    Update countingValleys.go

--- countingValleys.go
+++ countingValleys.go
@@ -1,12 +1,8 @@
+// Run it in go playground
 package main
 
 import (
-    "bufio"
     "fmt"
-    "io"
-    "os"
-    "strconv"
-    "strings"
 )
 
 // Complete the countingValleys function below.
@@ -23,47 +19,16 @@ func countingValleys(n int32, s string) int32 {
         if lvl == 0 && string(s[i]) == "U" {
             count += 1
         }
-        fmt.Println(lvl)
     }
-    fmt.Print(lvl)
 
     return count
 }
 
 func main() {
-    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
-
-    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-    checkError(err)
-
-    defer stdout.Close()
-
-    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
-
-    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
-    checkError(err)
-    n := int32(nTemp)
-
-    s := readLine(reader)
+    n := int32(8)
+    s := "DDUUDDD"
 
     result := countingValleys(n, s)
 
-    fmt.Fprintf(writer, "%d\n", result)
-
-    writer.Flush()
-}
-
-func readLine(reader *bufio.Reader) string {
-    str, _, err := reader.ReadLine()
-    if err == io.EOF {
-        return ""
-    }
-
-    return strings.TrimRight(string(str), "\r\n")
-}
-
-func checkError(err error) {
-    if err != nil {
-        panic(err)
-    }
+    fmt.Print(result)
 }
